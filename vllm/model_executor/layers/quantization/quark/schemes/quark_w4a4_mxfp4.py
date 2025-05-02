5f8b8cf9dd06d034bc2d4576cdaf2a4922b9fb03
Matthew Wong <Matthew.Wong2@amd.com>
2025-05-02 19:48:25 +0000

    Integrate AITER mxfp4 dynquant and GEMM kernels for gfx950
    Linting and relocate missing package warning to initialization
    
    Signed-off-by: Matthew Wong <Matthew.Wong2@amd.com>
    
    Enable PA custom and skinny GEMMs on gfx950; slight tweaks to FP4 GEMM and quant kernels
    
    Signed-off-by: Matthew Wong <Matthew.Wong2@amd.com>
    
    Enable one more gfx950 skinny gemm class
    
    Signed-off-by: Matthew Wong <Matthew.Wong2@amd.com>
    
    [Bugfix] Only transpose scales for non-emulation MXFP4 GEMM
    
    Signed-off-by: Matthew Wong <Matthew.Wong2@amd.com>

--- vllm/model_executor/layers/quantization/quark/schemes/quark_w4a4_mxfp4.py
+++ vllm/model_executor/layers/quantization/quark/schemes/quark_w4a4_mxfp4.py
@@ -14,7 +14,11 @@ from vllm.model_executor.parameter import (GroupQuantScaleParameter,
                                            PackedvLLMParameter)
 from vllm.platforms import current_platform
 
-logger = init_logger(__name__)
+try:
+    from aiter.ops.triton.gemm_afp4wfp4 import gemm_afp4wfp4
+    from aiter.ops.triton.quant import dynamic_mxfp4_quant
+except ImportError:
+    dynamic_mxfp4_quant = gemm_afp4wfp4 = None
 
 __all__ = ["QuarkW4A4MXFP4"]
 
@@ -27,29 +31,14 @@ class QuarkW4A4MXFP4(QuarkScheme):
         self.qscheme = "per_group"
         self.weight_quant_spec = weight_quant_spec
         self.input_quant_spec = input_quant_spec
-
-        self.static_input_scales = not input_quant_spec.get("is_dynamic")
-
-        if self.static_input_scales:
+        self.emulate = not current_platform.supports_mx()
+        if not self.emulate and (dynamic_mxfp4_quant is None
+                                 or gemm_afp4wfp4 is None):
+            # Currently need these kernels if not emulating
             raise NotImplementedError(
-                "QuarkW4A4MXFP4 with static input scales is currently not "
-                "implemented. Please open an issue.")
-
-        if not current_platform.supports_mx():
-            self.emulate = True
-            logger.warning_once(
-                "The current platform does not support native MXFP4 "
-                "computation. Simulated weight dequantization and activation "
-                "QDQ (quantize and dequantize) will be used, with the linear "
-                "layers computed in high precision.")
-        else:
-            self.emulate = True
-            logger.warning_once(
-                "The current platform supports native MXFP4 "
-                "computation, but kernels are not yet integrated in vLLM. "
-                "Simulated weight dequantization and activation "
-                "QDQ (quantize and dequantize) will be used, with the linear "
-                "layers computed in high precision.")
+                f"{self.__class__.__name__} requires AITER to be installed "
+                "for non-emulation mode! Please refer to "
+                "https://github.com/ROCm/aiter for installation details.")
 
     @classmethod
     def get_min_capability(cls) -> int:
@@ -58,8 +47,48 @@ class QuarkW4A4MXFP4(QuarkScheme):
     def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
         layer.weight = torch.nn.Parameter(layer.weight.data,
                                           requires_grad=False)
-        layer.weight_scale = torch.nn.Parameter(layer.weight_scale.data,
-                                                requires_grad=False)
+
+        if self.emulate:
+            layer.weight_scale = torch.nn.Parameter(layer.weight_scale.data,
+                                                    requires_grad=False)
+            try:
+                from quark.torch.export.nn.modules import realquantizer
+                from quark.torch.quantization.config.config import (
+                    QuantizationSpec)
+            except ImportError as err:
+                raise ImportError(
+                    "The package `amd-quark` is required to use AMD Quark "
+                    "MX-FP4 models. Please install it with `pip install "
+                    "amd-quark`.") from err
+
+            weight_quant_spec = QuantizationSpec.from_dict(
+                self.weight_quant_spec)
+
+            weight_quantizer = realquantizer.get_real_quantizer(
+                qspec=weight_quant_spec,
+                quantizer=None,
+                real_quantized=True,
+                reorder=False,
+                float_dtype=self.out_dtype,
+                scale_shape=layer.weight_scale.shape,
+                zero_point_shape=None,
+            )
+            weight_quantizer.scale.data = layer.weight_scale.data
+
+            if not envs.VLLM_QUARK_EMU_MEM_OPT:
+                layer.weight = torch.nn.Parameter(
+                    weight_quantizer(layer.weight.data).to(self.out_dtype),
+                    requires_grad=False,
+                )
+            else:
+                self.weight_quantizer = weight_quantizer
+            layer.weight_scale = None
+
+            # This call is necessary to release the scales memory.
+            torch.cuda.empty_cache()
+        else:
+            layer.weight_scale = torch.nn.Parameter(
+                layer.weight_scale.data.T.contiguous(), requires_grad=False)
 
     def create_weights(self, layer: torch.nn.Module,
                        output_partition_sizes: list[int],
@@ -109,4 +138,11 @@ class QuarkW4A4MXFP4(QuarkScheme):
 
             return F.linear(x, dq_w, bias)
         else:
-            raise NotImplementedError()
+            x_q, x_s = dynamic_mxfp4_quant(x)
+            y = torch.empty(x_q.shape[0],
+                            layer.weight.shape[0],
+                            device=x_q.device,
+                            dtype=self.out_dtype)
+            gemm_afp4wfp4(x_q, layer.weight.T, y, x_s, layer.weight_scale.T,
+                          self.out_dtype)
+            return y
